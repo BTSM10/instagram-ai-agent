17ffacc7ecdfd497ec32eb0c6ab8281a8f3b8e0b
BTSM10 <biniamtsiget@gmail.com>
2026-06-03 16:09:45 +0300

    Remove fast folder — moved to FastAPI_Project repo

--- fast/TodoApp/database.py
+++
@@ -1,8 +0,0 @@
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
-from sqlalchemy.orm import declarative_base
-
-SQL_ALCHEMY_URL ="sqlite:///./todoapp.db"
-engine = create_engine(SQL_ALCHEMY_URL, connect_args={"check_same_thread": False})
-SessionLocal = sessionmaker(autocommit= False, autoflush=False, bind = engine)
-base = declarative_base()
